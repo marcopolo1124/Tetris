057268932247e60b18d74279615c3d80cc5ea81c
marcopolo1124 <85742840+marcopolo1124@users.noreply.github.com>
2021-08-09 11:28:16 +0400

    The first block class
    Instantiates with all rotations, and can generate an array with the blocks in physical form

---
+++ Tetris_new.py
@@ -0,0 +1,78 @@
+import numpy as np
+
+class block:
+    
+    def __init__(self, center, color, default_coord):
+        self.center = np.array(center)
+        self.color = color
+        self.default_coord = []
+        self.rotation_dict={}
+
+        #Set coordinate to numpy array
+        for point in default_coord:
+            self.default_coord.append(np.array(point))
+
+        #Create a dictionary of all rotation    
+        self.gen_rotation_dict()
+
+    def __repr__(self):
+        return self.color
+
+    def rotate(self, turn = 0):
+
+        #Rotation matrices
+        identity = np.array([[1,0],
+                             [0,1]])
+        left = np.array([[0, -1],
+                        [1, 0]])
+        half = np.array([[-1,0],
+                        [0,-1]])
+        right = np.array([[0,1],
+                          [-1,0]])
+        
+        #Allow for easier access to the matrices through integers
+        rotation_dict = { 0: identity, 1: right, 2: half, 3: left}
+        
+        new_coord = []
+        for point in self.default_coord:
+            new_point = np.dot(rotation_dict[turn],point-self.center)+self.center
+            new_coord.append(new_point)
+        return new_coord
+    
+
+    
+    #dictionary of all rotations of a block
+    def gen_rotation_dict(self):
+        for i in range(4):
+            self.rotation_dict[i] = self.rotate(i)
+
+            
+    #Shows the blocks in a physical format. Can be further used
+    def list_representation(self, turn):
+        arr = [[' ' for i in range(4)] for j in range(4)]
+        for i,j in self.rotation_dict[turn]:
+            arr[int(j)][int(i)] = self.color
+
+        for i in range(3,-1,-1):
+            print(arr[i])
+        return arr
+    
+    def show_all(self):
+        for i in range(4):
+            self.list_representation(i)
+            print(' ')
+
+
+I = block((1.5,1.5), 'B', [[0,2], [1,2], [2,2], [3,2]])
+J = block((1,1), 'D', [[0,1], [1,1], [2,1], [0,2]])
+L = block((1,1), 'O', [[0,1], [1,1], [2,1], [2,2]])
+T = block((1,1), 'P', [[0,1], [1,1], [2,1], [1,2]])
+O = block((1.5,1.5), 'Y', [[1,1], [2,1], [1,2], [2,2]])
+S = block((1,1), 'G', [[0,1], [1,1], [1,2], [2,2]])
+Z = block((1,1), 'R', [[1,1], [2,1], [0,2], [1,2]])
+
+block_lst = [I,J,L,T,O,S,Z]
+
+for block in block_lst:
+    block.show_all()
+
